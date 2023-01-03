ce9543f47797aab78e286e2f46095248688e0bb3
Ashish Sengar <techie.sengar1924@gmail.com>
2023-01-03 19:36:23 +0530

    final

--- Monu Bhaiya/Delete_Coloumn_To_Make_Store_944.java
+++ Monu Bhaiya/Delete_Coloumn_To_Make_Store_944.java
@@ -5,7 +5,24 @@ public class Delete_Coloumn_To_Make_Store_944 {
 
     public static int Find(String ans[])
     {
-        
+
+        int sum = 0;
+
+        for(int i = 0 ; i < ans[0].length() ; i++)
+        {
+            for(int j = 1 ; j < ans.length ; j++)
+            {
+                if(ans[j].charAt(i) < ans[j-1].charAt(i))
+                {
+                    // return i;
+                    sum++;
+                    break;
+
+                }
+
+            }
+        }
+        return sum;
     }
 
 
@@ -16,6 +33,10 @@ public class Delete_Coloumn_To_Make_Store_944 {
         Scanner sc = new Scanner(System.in);
 
         String str[] = {"abc", "bce", "cae"};
+
+        // String str[] = {"zyx","wvu","tsr"};
+
+        System.out.println(Find(str));
 
     }
     
