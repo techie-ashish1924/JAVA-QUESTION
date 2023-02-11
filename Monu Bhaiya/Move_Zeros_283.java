1bd7851d5d5171d2acec428b8d75ffde68903afa
Ashish Sengar <techie.sengar1924@gmail.com>
2023-02-11 21:41:17 +0530

    final

---
+++ Monu Bhaiya/Move_Zeros_283.java
@@ -0,0 +1,3 @@
+public class Move_Zeros_283 {
+    
+}
--- Monu Bhaiya/String_To_Integer_8.java
+++ Monu Bhaiya/String_To_Integer_8.java
@@ -25,8 +25,8 @@ public class String_To_Integer_8 {
         
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.next();
-
+        // String s = sc.next();
+        String s = "  -42";
         // String s = "--42";
         // String s = "1234 words i love ypu 856484";
 
@@ -34,8 +34,8 @@ public class String_To_Integer_8 {
 
         int flag = 0;
         String f = "";
-        int fDigit_ind = Integer.MAX_VALUE;
         int sum = 0;
+        s = s.replaceAll("\\s", "");
 
         for (int i = 0; i < s.length(); i++) {
 
@@ -68,16 +68,21 @@ public class String_To_Integer_8 {
 
         }
         
-        System.out.println(f);
+        // System.out.println("String is : " + f + " ans " + f.length());
 
-        // if (s.charAt(fDigit_ind-1) == '-') {
-        //     // return Find(f.substring(1)) * -1;
-        //     System.out.println(Find(f.substring(1)) * -1);
-        // } else {
-        //     // return Find(f);
-        //     System.out.println(Find(f));
-        // }
-        
-        // System.out.println(f);
+        if (f.length() == 0) {
+            // return 0;
+            System.out.println(0);
+        }
+
+        else if (f.charAt(0) == '-') {
+            // return Find(f.substring(1)) * -1;
+            System.out.println(Find(f.substring(1)) * -1);
+        } else {
+            // return Find(f);
+            System.out.println(Find(f));
+        }
+
+       
     }
 }
